3f36c2da4be2c4423bbbd242d5a4dcc9443b581e
Kelmik91 <kelmik91@me.com>
2024-04-05 00:49:46 +0300

    Инкремент 4:fix name flags in config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,8 +3,8 @@ package config
 import "flag"
 
 var (
-	Host       = flag.String("-a", "localhost:8080", "адрес запуска HTTP-сервера")
-	BaseUrlRes = flag.String("-b", "http://localhost:8000/", "базовый адрес результирующего сокращённого URL")
+	Host       = flag.String("a", "localhost:8080", "адрес запуска HTTP-сервера")
+	BaseUrlRes = flag.String("b", "http://localhost:8080/", "базовый адрес результирующего сокращённого URL")
 )
 
 func ParseFlags() {
