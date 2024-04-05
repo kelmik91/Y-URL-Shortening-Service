8f3c1c8af7dcc74af0088b6caef7719e3cdd36a7
Kelmik91 <kelmik91@me.com>
2024-04-06 00:01:24 +0300

    Инкремент 5

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,24 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var (
-	Host       = flag.String("a", "localhost:8080", "адрес запуска HTTP-сервера")
-	BaseUrlRes = flag.String("b", "http://localhost:8080", "базовый адрес результирующего сокращённого URL")
+	Host       string
+	BaseUrlRes string
 )
 
 func ParseFlags() {
+	flag.StringVar(&Host, "a", "localhost:8080", "адрес запуска HTTP-сервера")
+	flag.StringVar(&BaseUrlRes, "b", "http://localhost:8080", "базовый адрес результирующего сокращённого URL")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
+		Host = envRunAddr
+	}
+	if envBaseAddr := os.Getenv("BASE_URL"); envBaseAddr != "" {
+		BaseUrlRes = envBaseAddr
+	}
 }
